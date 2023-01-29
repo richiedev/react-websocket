f9908f43e19665eb577819833d1d822d29360dd8
richiedev <richieprojektz@gmail.com>
2023-01-29 11:18:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import socketIO from 'socket.io-client';
 import './App.css';
 
+const socket = socketIO.connect('http://localhost:4000');
+
 function App() {
+  const [terminals, setTerminals] = useState([]);
+  // const [terminalSessionId] = useState(1);
+  const [terminalSessionId] = useState(Math.floor(Math.random() * 10));
+
+  useEffect(() => {
+    socket.emit('saveid', { id: terminalSessionId });
+
+    // socket.on('showchargemodal', (data) => alert('Pay now....', data));
+    socket.on(`showchargemodal/5`, (data) => {
+    // socket.on(`showchargemodal/${terminalSessionId}`, (data) => {
+      console.log('Pay now....', data);
+      alert(data.amount)
+    });
+
+    // socket.on('sortoutpayment', (data) => {
+    //   console.log('Pay now....', data);
+    //   alert(data.amount)
+    // });
+  }, []);
+
+  const getTerminals = () => {
+    fetch('http://localhost:5000/listofterminals')
+      .then(data => data.json())
+      .then(data => setTerminals(data));
+  }
+
+  const sortOutPayment = (id) => {
+    fetch('http://localhost:5000/terminalpayment/', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({ id, orderId: '123445678' })
+    })
+      .then(data => data.json())
+      .then(data => setTerminals(data));
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getTerminals}>
+          Card
+        </button>
+
+        <ul>
+         {terminals.map((terminal) => (
+          <li
+            style={{cursor: 'pointer'}}
+            // onClick={() => socket.emit('cardpayment', { data: 'carddata '})}
+            onClick={() => sortOutPayment(terminal)}
+          >
+            {terminal}
+          </li>
+         ))}
+
+        </ul>
       </header>
     </div>
   );
